1c063ed094298548c4c42c60e0327f157b30229d
Funminiyi Adedoyin <doyinfummi2012@gmail.com>
2021-11-14 16:54:46 +0100

    working with Angular

---
+++ app/app.js
@@ -0,0 +1,77 @@
+var myNewApp = angular.module('myAngularProject', []);
+
+
+myNewApp.controller('SchoolController', ['$scope', function($scope){
+    $scope.message= "Hello world!";
+    $scope.NewMessage = "hey y'all";
+    
+
+    // adding a new Dev
+    $scope.newDev = function(boy) {
+        $scope.boys.push({
+            name: $scope.newdev.name,
+        });
+    }
+
+
+    $scope.schools = ['Adekunle Ajasin', 'TASUED', 'Babcock ','Lasu'];
+    $scope.menuList= ["Rice", "jollof", "cocktail"];
+
+    $scope.boys=
+    [  {
+        name: "Joshua",
+        role: "Frontend Developer",
+        tag: "green"
+        },
+
+        {
+            name: "John",
+            role: "Backend Developer",
+            tag: "violet"
+        },
+
+        {
+            name: "Joshua",
+            role: "Product Manager",
+            tag: "grey"
+        },
+        {
+            name: "Robin",
+            role: "Data Scientist",
+            tag: "violet"
+        },
+
+        {
+            name: "Kehinde",
+            role: "Product Manager",
+            tag: "grey"
+        },
+
+        {
+            name: "Usman",
+            role: "Data Analyst",
+            tag: "pink"
+        },
+
+        {
+            name: "Moses",
+            role: "Software Tester",
+            tag: "black"
+        },
+
+        {
+            name: "Adeyanju",
+            role: "Software Tester",
+            tag: "brown"
+        },
+        {
+            name: "Damilola",
+            role: "Frontend Developer",
+            tag: "grey"
+        }
+
+
+    ]
+
+}]);
+
