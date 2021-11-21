7704dfa22d55ccb3a155498ffd4fb4616f269d85
Funminiyi Adedoyin <doyinfummi2012@gmail.com>
2021-11-21 09:42:36 +0100

    figuring something out

--- app/app.js
+++ app/app.js
@@ -5,7 +5,8 @@ myNewApp.config(['$routeProvider', function($routeProvider){
 
 $routeProvider
     .when('/home', {
-    templateUrl: 'views/home.html'
+    templateUrl: 'views/home.html',
+    controller: 'devController'
 })
     .when('/devs', {
     templateUrl: 'views/devs.html',
@@ -19,8 +20,55 @@ $routeProvider
 
 
   
+// Custom Directive
+myNewApp.directive('randomDev', [function(){
+// restrict consists of 4 letters: A-Attribute,E-Element,C-classname,M-Comment-. To specify how we want our directive to be used. 
+    return{
+        restrict: 'E',
+        scope:{
+            devs: '=',
+            title: '='
+
+        },
+
+        // the template will output whatever is in it.
+
+       //not working 
+        // template: '<img ng-src="{{devs[0].thumb}}">',
+
+        // templateurl can be used here too to go to a new view
+        templateUrl: views/random.html,  
+
+        // to include html from the home.html into random.html, Use the transclude property here, then type the text we want into the home.html but it won't still show until the ng-transclude is used in the random.html on any element, so anything typed into the home.html will go into the element with the ng-transclude .
+        transclude: true,
+        // to replace the custom directive with an html tag as soon as it's displayed on the browser.The replace property will replace the custom directive with the outermost tag/ its parent tag name instead.
+        // not working
+        // replace: true,
+
+        controller: function($scope){
+         // to get random images
+            // $scope.random = Math.floor(Math.random()*4)
+        }
+    };
+}]);
 
 
+// another custom directive
+// myNewApp.directive('devName', [function(){
+//     return{
+//         restrict: 'E',
+//         scope:{
+//             devs: '=',
+//             title: '='
+//         },
+
+//         template: '{{title}}',
+
+//         controller: function($scope){
+            
+//         }
+//     }
+// }]);
 
 myNewApp.controller('devController', ['$scope', '$http', function($scope, $http){    
 
@@ -109,10 +157,15 @@ myNewApp.controller('devController', ['$scope', '$http', function($scope, $http)
     // to use json:Make sure to use this inside the controller and not outside
 // console.log(angular.toJson($scope.devs));
 
-$http.get('data/devs.json').success(function(data){
+// use .then  and not .success bcos of the version of angular higher than 1.4.3
+
+$http.get('data/devs.json').then(function(data){
 
     $scope.devs = data;
 });
+
+
+
 }]);
 
 
